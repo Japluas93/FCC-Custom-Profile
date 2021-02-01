6fa48ec91b4d965193f37e4a1cf02c41f88fc8c3
Julian Pluas <julianpluas@gmail>
2021-01-31 16:51:07 -0800

    Fixed a bug

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const routes = require("./controllers/something_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
