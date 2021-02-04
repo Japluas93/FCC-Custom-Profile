8984d811233a8107c0bdf9ceb6891b10a0a85534
Ocampo <mariaocampo0513@gmail.com>
2021-02-04 13:40:15 -0800

    uncommented code blocks for routes

--- controllers/customs_controller.js
+++ controllers/customs_controller.js
@@ -1,30 +1,30 @@
-// constdb=require('../common/models');
+constdb=require('../common/models');
 
 exports.profile = (req, res)=>{
   res.render("./profile/home")
 };
-// exports.profile = function(req, res) {
-//   db.Custom.findOne ({
-//     where: {
-//       UserId: req.user.id
-//     }
-//   }).then(function(dbCustom) {
-//     console.log(dbCustom);
-//     res.render('customs/customs', {
-//       layout: 'main-customs',
-//       custom: dbCustom
-//     });
-//   });
-// };
+exports.profile = function(req, res) {
+  db.Custom.findOne ({
+    where: {
+      UserId: req.user.id
+    }
+  }).then(function(dbCustom) {
+    console.log(dbCustom);
+    res.render('customs/customs', {
+      layout: 'main-customs',
+      custom: dbCustom
+    });
+  });
+};
 
-// exports.createProfile = function(req, res) {
+exports.createProfile = function(req, res) {
 
-//   console.log(req.user);
-//   // Add id from User onto req.body
-//   req.body.UserId = req.user.id;
+  console.log(req.user);
+  // Add id from User onto req.body
+  req.body.UserId = req.user.id;
 
-//   db.Custom.create(req.body).then(function(dbPost) {
-//     res.json(dbPost);
-//   });
-// };
+  db.Custom.create(req.body).then(function(dbPost) {
+    res.json(dbPost);
+  });
+};
 
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router  = express.Router();
 
-// var passport = require("../config/passport");
+var passport = require("../config/passport");
 const users_controller = require('../controllers/users_controller');
-// var isAuthenticated = require("../config/middleware/isAuthenticated");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 
 router.get('/registration', users_controller.registrationPage);
 
-// router.get('/sign-out', users_controller.signOutUser);
+router.get('/sign-out', users_controller.signOutUser);
 
-// router.post('/login', passport.authenticate("local"), users_controller.loginUser);
+router.post('/login', passport.authenticate("local"), users_controller.loginUser);
 
-// router.post('/signup', users_controller.signUpUser);
+router.post('/signup', users_controller.signUpUser);
 
 module.exports = router;
 
-//github sucks
